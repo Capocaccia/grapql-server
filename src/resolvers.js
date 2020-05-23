6cea73efaa24e1d94da85b0842eb6949bcdd9d24
Carter Capocaccia <carter.capocaccia@gmail.com>
2020-05-23 13:29:12 -0500

    adds resolvers for crud operations on users

--- src/resolvers.js
+++ src/resolvers.js
@@ -8,6 +8,29 @@ const resolvers = {
         users: (parent, args, context, info) => {
           return users;
         }
+      },
+      Mutation: {
+          createUser: (parent, {id, name, email, age}, context, info) => {
+            const newUser = { id, name, email, age}
+            users.push(newUser)
+            return newUser;
+          },
+          updateUser: (parent, {id, name, email, age}, context, info) => {
+            const userToUpdate = users.find((user) => user.id === id)
+            userToUpdate.name = name
+            userToUpdate.email = email
+            userToUpdate.age = age
+            return userToUpdate
+          },
+          deleteUser: (parent, {id}, context, info) => {
+            const userIndex = users.findIndex(user => user.id === id);
+
+            if (userIndex === -1) throw new Error("User not found.");
+
+            const deletedUsers = users.splice(userIndex, 1);
+
+            return deletedUsers[0];
+          }
       }
 };
 
